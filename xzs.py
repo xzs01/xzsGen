22a3169227c2967e99cf79b6e3bd6238b2c67f8b
xzs01 <108091782+xzs01@users.noreply.github.com>
2022-06-23 19:40:35 +0100

    Create xzs.py

---
+++ xzs.py
@@ -0,0 +1,84 @@
+import time
+import colorama
+from colorama import Fore
+print(
+  f"""
+  {Fore.RED}
+╔═══════════════════════════════════════╗
+║ __  __ _________     ____ _____ _   _ ║
+║ \ \/ /|__  / ___|   / ___| ____| \ | |║
+║  \  /   / /\___ \  | |  _|  _| |  \| |║
+║  /  \  / /_ ___) | | |_| | |___| |\  |║
+║ /_/\_\/____|____/   \____|_____|_| \_|║
+╚═══════════════════════════════════════╝
+    ╔═══════════════════════╗
+    ║   Welcome,  Person    ║
+    ╚═══════════════════════╝
+    ╔═══════════════════════╗
+    ║1) 2022 F-BIBLE        ║
+    ║2) 2020 F-BIBLE        ║  
+    ║3) DOMINOS METHOD      ║
+    ║4) 2019 F-BIBLE        ║
+    ║5) CREATOR INFO        ║
+    ║6) DISCORD SERVER      ║
+    ║7) COMING SOON         ║
+    ║8) CLOSE SOFTWARE      ║
+    ║9) AMAZON SC GEN       ║
+    ╚═══════════════════════╝
+  """
+)
+while True:
+  answer = input(
+    f"""
+    {Fore.RED}
+
+{Fore.RESET}
+    """
+)
+  if answer =="1":
+     answer = print(
+       """
+       Fraud bibles v1, v2, v3
+
+https://mega.nz/folder/fS5iAZrD#S_DXCwo9YT4BiqE_c_4n-A
+https://mega.nz/folder/HOI1jS4C#Himp0Xqk1SyfA7HIOzQuoA
+https://mega.nz/folder/LbR0EBCA#X-4Tw-jc5wOPd9Ds4LoAMw
+       """
+     )
+  if answer =="2":
+      answer = print("? Fraud Bible 2020 Link - https://mega.nz/folder/CwxjhLbT#VKc3T2NduWqeyEd5E5SUMQ ")
+  if answer =="3":
+      answer = print(
+        f"""
+  {Fore.WHITE}
+Not sure if a method like this has been posted before but i did search about. Anyways, contact dominoes head office 
+(UK) - http://www.dominos.co.uk/about/contactus.aspx
+(US) - https://www.dominos.com/en/pages/services/
+Say that you went to X store & ordered X pizzas for takeaway & once you got home & opened the box it was burned or use whatever excuse you feel comfortable with & wait for a reply. They should then email back saying they will pass the details on to the store that you mentioned going to. 
+
+Then you will receive another email apologizing & offering you 2 free large pizzas or something similar. Print this off a take it to the store for your free pizzas, or better yet just phone your local dominoes & order them for delivery & hand the printed email to the driver.
+        """
+)
+  if answer =="4":
+      answer = print(
+        """
+? Fraud Bible 2019 Link - https://mega.nz/folder/O0ZRyQpb#xpT_rBTvNfCzo7y9CILY1g 
+ 
+? Fraud Bible 2019 Link 2 - https://mega.nz/#F!O0ZRyQpb 
+decryption key: xpT_rBTvNfCzo7y9CILY1g
+        """
+      )
+  if answer =="5":
+      answer = print("DISCORD USER = xzs#001")
+  if answer =="6":
+      answer = print("DISCORD SERVER = discord.gg/xzs")
+  if answer =="8":
+    exit()
+  if answer =="7":
+     answer = print("COMING SOON....")
+  if answer =="8":
+   exit()
+  if answer =="9":
+   from test.test import myfunc 
+myfunc()
+time.sleep(300)

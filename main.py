8c30fba1e71826a6fe3d4dfa1328889f9eb270ce
xzs01 <108091782+xzs01@users.noreply.github.com>
2022-06-23 19:38:52 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import colorama
+from colorama import Fore
+while True:
+  answer = input(
+    f"""
+    {Fore.RED}
+╔═════════════════════════════╗
+║   type your key to login    ║
+╚═════════════════════════════╝
+{Fore.RESET}
+    """
+)
+  if answer =="nsTfbgEvSTb":
+   from xzs.xzs import myfunc 
+myfunc()
